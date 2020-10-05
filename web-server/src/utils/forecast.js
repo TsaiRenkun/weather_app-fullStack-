839cfcbebcfa8aa9adfa368647f19dab992bd6bf
Tsairenkun <Tsairenkun@gmail.com>
2020-10-05 13:14:12 +0800

    Completed node weather app

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lat, long, callback) => {
     } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
-      callback(undefined, body.current);
+      callback(undefined,  ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.');
     }
   });
 };
